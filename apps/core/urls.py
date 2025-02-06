66d269b0d8de25729d939c74ac67c963e9994621
Muli Job <munyoki912@gmail.com>
2025-02-06 23:23:27 +0300

    user registration and login

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,7 +1,11 @@
 """Urls Pattern"""
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-  path('', views.homepage, name='home')
+  path('', views.homepage, name='home'),
+  path('signup/', views.signup, name='signup'),
+  path('login/', LoginView.as_view(template_name='core/login.html'), name='login'),
+  path('logout/', LogoutView.as_view(), name='logout'),
 ]
