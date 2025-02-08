b8bf6ac07dec826a7bdbca98236efaa4e22fe76c
Muli Job <munyoki912@gmail.com>
2025-02-09 00:42:21 +0300

    Created function to view a single category detail and adding a category

--- apps/bookmark/views.py
+++ apps/bookmark/views.py
@@ -1,8 +1,49 @@
 """Bookmark View Functions"""
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from .models import Category
+from .forms import CategoryForm
+
 @login_required
-def category(request):
+def categories(request):
     """Bookmark categories"""
-    return render(request, 'bookmark/categories.html')
+    categories = request.user.categories.all()
+
+    context = {
+        "categories": categories
+    }
+    return render(request, 'bookmark/categories.html', context)
+
+@login_required
+def category(request, category_id):
+    """Category detail"""
+    category = Category.objects.get(pk=category_id)
+
+    context = {
+        "category": category
+    }
+
+    return render(request, 'bookmark/category.html', context)
+
+
+@login_required
+def category_add(request):
+    """Adding a category"""
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            category = form.save(commit=False)
+            category.created_by = request.user
+            category.save()
+
+            return redirect('categories')
+    else:
+        form = CategoryForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, 'bookmark/category_add.html', context)
--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from apps.bookmark.views import category
+from apps.bookmark.views import categories, category, category_add
 from . import views
 
 urlpatterns = [
   path('', views.dashboard, name='dashboard'),
-  path('categories/', category, name='category'),
+  path('categories/', categories, name='categories'),
+  path('categories/<int:category_id>/', category, name='category'),
+  path('add-category/', category_add, name='category_add'),
 ]
